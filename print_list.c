24ad8c92eaa4e371fef07eea15699cdc5a11b3a4
mupindude <mupindudevine@gmail.com>
2026-06-22 07:58:36 +0400

    Just writting some tests

--- print_list.c
+++ print_list.c
@@ -1,15 +1,24 @@
 #include "list.h"
 #include <unistd.h>
 
+void ft_pustr(char *s)
+{
+	int i = 0;
+	while(s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
 void print_list(t_list *head)
 {
-	if (!head)
-		return ;
-	int i = 0;
-	while(head->next)
+	while (head)
 	{
-		write(1, &head[i], 1);
-		i++;
+		ft_pustr((char *)head->data);
+		if (head->next)
+			write(1, "->", 2);
 		head = head->next;
 	}
+	write(1, "-> NULL\n", 8);
 }
